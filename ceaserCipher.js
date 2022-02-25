b5e07645a06f0490f66d54110209044a0b98e5f9
Adriel <adriel.fariasan@gmail.com>
2022-02-25 10:28:27 -0300

    Adiciona funcao para calcular novo index

--- ceaserCipher.js
+++ ceaserCipher.js
@@ -1,3 +1,13 @@
 function ceaserCipher(text, shift) {}
 
+function novoIndex(alfabeto, letra, shift) {
+  let index = alfabeto.indexOf(letra) + shift;
+  if (index < 0) {
+    return index + 26;
+  } else if (index > 25) {
+    return index - 26;
+  }
+  return index;
+}
+
 module.exports = ceaserCipher;

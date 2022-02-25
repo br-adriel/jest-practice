7ba9a11a3dae24410d634924c78b41082d35f2e3
Adriel <adriel.fariasan@gmail.com>
2022-02-25 10:29:03 -0300

    Implementa funcao ceaserCipher

--- ceaserCipher.js
+++ ceaserCipher.js
@@ -1,4 +1,21 @@
-function ceaserCipher(text, shift) {}
+function ceaserCipher(text, shift) {
+  const alfabetoMin = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alfabetoMai = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  let texto = text.split("");
+  texto = texto.map((letra) => {
+    if (alfabetoMin.indexOf(letra) !== -1) {
+      index = novoIndex(alfabetoMin, letra, shift);
+      return alfabetoMin[index];
+    } else if (alfabetoMai.indexOf(letra) !== -1) {
+      index = novoIndex(alfabetoMai, letra, shift);
+      return alfabetoMai[index];
+    } else {
+      return letra;
+    }
+  });
+  return texto.join("");
+}
 
 function novoIndex(alfabeto, letra, shift) {
   let index = alfabeto.indexOf(letra) + shift;

fa014bfa22dcbefe5267d85247bf4dad19c66ba4
Adriel <adriel.fariasan@gmail.com>
2022-02-25 12:05:27 -0300

    Implementa funcao analyzeArray

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,3 +1,21 @@
-function analyzeArray(arr) {}
+function analyzeArray(arr) {
+  let length = arr.length;
+  let soma = 0;
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let item of arr) {
+    soma += item;
+    max = item > max ? item : max;
+    min = item < min ? item : min;
+  }
+
+  return {
+    average: soma / length,
+    max,
+    min,
+    length,
+  };
+}
 
 module.exports = analyzeArray;

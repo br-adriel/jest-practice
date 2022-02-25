d4d0d97d4d3b79833e867ba209e9de723a884d60
Adriel <adriel.fariasan@gmail.com>
2022-02-24 23:49:15 -0300

    Adiciona teste com palavra unica ja capitalizada

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require("./capitalize");
 test("Palavra", () => {
   expect(capitalize("palavra")).toBe("Palavra");
 });
+
+test("Palavra ja capitalizada", () => {
+  expect(capitalize("Palavra")).toBe("Palavra");
+});

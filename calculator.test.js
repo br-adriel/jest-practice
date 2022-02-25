2b17678bb3011e0a46e01186492b74512a578caf
Adriel <adriel.fariasan@gmail.com>
2022-02-25 10:03:57 -0300

    Adiciona teste de soma decimal

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require("./calculator");
 test("soma", () => {
   expect(calculator.add(4, 16)).toBe(20);
 });
+
+test("soma decimal", () => {
+  expect(calculator.add(4.2, 15.8)).toBeCloseTo(20);
+});

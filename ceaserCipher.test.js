829560786d06669b01ae718a565310055549b800
Adriel <adriel.fariasan@gmail.com>
2022-02-25 10:26:55 -0300

    Adiciona teste com shift de 26 posicoes

--- ceaserCipher.test.js
+++ ceaserCipher.test.js
@@ -3,3 +3,7 @@ const ceaserCipher = require("./ceaserCipher");
 test("5 casas", () => {
   expect(ceaserCipher("Bond, James Bond.", 5)).toBe("Gtsi, Ofrjx Gtsi.");
 });
+
+test("26 casas", () => {
+  expect(ceaserCipher("Bond, James Bond.", 26)).toBe("Bond, James Bond.");
+});

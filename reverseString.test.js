b169adc6da5106fff5edaaf4a71b166827e18d42
Adriel <adriel.fariasan@gmail.com>
2022-02-24 23:56:07 -0300

    Adiciona teste com frase

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require("./reverseString");
 test("Palavra", () => {
   expect(reverseString("Palavra!")).toBe("!arvalaP");
 });
+
+test("Frase", () => {
+  expect(reverseString("As palavras escritas!")).toBe("!satircse sarvalap sA");
+});
